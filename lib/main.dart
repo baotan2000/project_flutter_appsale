87ec96d7e11649957b92fc92fb50a7cc4695e5e8
baotan2000 <lamphucbaotan@gmail.com>
2023-08-10 11:04:06 +0700

    setup providers

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,26 @@
 import 'package:flutter/material.dart';
 import 'package:project_appsale/page/home/widget/home.dart';
+import 'package:project_appsale/providers/slider_provider.dart';
+import 'package:provider/provider.dart';
 
 void main(List<String> args) {
-  runApp(MaterialApp(
-    debugShowCheckedModeBanner: false,
-    initialRoute: HomePage.routerName,
-    routes: {
-      HomePage.routerName: (context) => HomePage(),
-    },
-    // home: MyApp(),
-  ));
+  runApp(
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (_) => SliderProvider(),
+        ),
+      ],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        initialRoute: HomePage.routerName,
+        routes: {
+          HomePage.routerName: (context) => HomePage(),
+        },
+        // home: MyApp(),
+      ),
+    ),
+  );
 }
 
 // class MyApp extends StatelessWidget {
