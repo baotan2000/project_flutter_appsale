4675cddc69fcd8b277c6e445ad3f2584dcb067e2
baotan2000 <lamphucbaotan@gmail.com>
2023-08-10 11:38:00 +0700

    http

--- lib/page/home/widget/home_slider.dart
+++ lib/page/home/widget/home_slider.dart
@@ -1,32 +1,54 @@
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
+import 'package:project_appsale/providers/slider_provider.dart';
+import 'package:provider/provider.dart';
 
-class HomeSlider extends StatelessWidget {
+class HomeSlider extends StatefulWidget {
   const HomeSlider({super.key});
+
+  @override
+  State<HomeSlider> createState() => _HomeSliderState();
+}
+
+class _HomeSliderState extends State<HomeSlider> {
+  late Future sliderFuture;
+
+  @override
+  void didChangeDependencies() {
+    sliderFuture = Provider.of<SliderProvider>(context).getSlider();
+    // TODO: implement didChangeDependencies
+    super.didChangeDependencies();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.symmetric(vertical: 15, horizontal: 10),
-      child: CarouselSlider(
-        options: CarouselOptions(
-          height: 200.0,
-          autoPlay: true,
-        ),
-        items: [1, 2, 3, 4, 5].map((i) {
-          return Builder(
-            builder: (BuildContext context) {
-              return Container(
-                  width: MediaQuery.of(context).size.width,
-                  margin: EdgeInsets.symmetric(horizontal: 5.0),
-                  decoration: const BoxDecoration(color: Colors.amber),
-                  child: Text(
-                    'text $i',
-                    style: TextStyle(fontSize: 16.0),
-                  ));
-            },
+      child: FutureBuilder(
+        future: sliderFuture,
+        initialData: [],
+        builder: (context, asyncData) {
+          return CarouselSlider(
+            options: CarouselOptions(
+              height: 200.0,
+              autoPlay: true,
+            ),
+            items: [1, 2, 3, 4, 5].map((i) {
+              return Builder(
+                builder: (BuildContext context) {
+                  return Container(
+                      width: MediaQuery.of(context).size.width,
+                      margin: EdgeInsets.symmetric(horizontal: 5.0),
+                      decoration: const BoxDecoration(color: Colors.amber),
+                      child: Text(
+                        'text $i',
+                        style: TextStyle(fontSize: 16.0),
+                      ));
+                },
+              );
+            }).toList(),
           );
-        }).toList(),
+        },
       ),
     );
   }
--- lib/providers/slider_provider.dart
+++ lib/providers/slider_provider.dart
@@ -1,3 +1,13 @@
-import 'package:flutter/material.dart';
+import 'dart:convert';
 
-class SliderProvider extends ChangeNotifier {}
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+class SliderProvider extends ChangeNotifier {
+  Future<void> getSlider() async {
+    final url = 'http://apiforlearning.zendvn.com/api/mobile/sliders';
+    final response = await http.get(Uri.parse(url));
+    final jsonData = jsonDecode(response.body);
+    print(jsonData);
+  }
+}
