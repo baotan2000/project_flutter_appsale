e509dd29c4ba3e5e6a6f597d4be9d4ab18f15a6e
baotan2000 <lamphucbaotan@gmail.com>
2023-08-16 12:37:44 +0700

    show cart

--- lib/page/cart/cart.dart
+++ lib/page/cart/cart.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart' as intl;
+import 'package:project_appsale/providers/cart_provider.dart';
+import 'package:provider/provider.dart';
 
 class CartPage extends StatelessWidget {
   static const routerName = '/cart';
@@ -14,41 +16,47 @@ class CartPage extends StatelessWidget {
       body: Stack(
         children: [
           Positioned.fill(
-            child: ListView.separated(
-                itemBuilder: (context, index) {
-                  return ListTile(
-                    leading: Image(
-                      fit: BoxFit.fill,
-                      image: NetworkImage(
-                        'https://images.unsplash.com/photo-1501162946741-4960f990fdf4?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1740&q=80',
-                      ),
-                    ),
-                    title: Text("Iphone 15 VIP PRO"),
-                    subtitle: Text(
-                        intl.NumberFormat.simpleCurrency(locale: 'vi')
-                            .format(20000000)),
-                    trailing: SizedBox(
-                      width: MediaQuery.of(context).size.width / 5,
-                      child: Row(
-                        children: [
-                          InkWell(onTap: () {}, child: Icon(Icons.remove)),
-                          Padding(
-                            padding: const EdgeInsets.symmetric(horizontal: 5),
-                            child: Text(
-                              "1",
-                              style: TextStyle(fontSize: 20),
-                            ),
+            child: Consumer<CartProvider>(
+              builder: (context, value, child) {
+                var dataItems = value.items.values.toList();
+                return ListView.separated(
+                    itemBuilder: (context, index) {
+                      return ListTile(
+                        leading: Image(
+                          fit: BoxFit.fill,
+                          image: NetworkImage(
+                            '${dataItems[index].image}',
                           ),
-                          InkWell(onTap: () {}, child: Icon(Icons.add))
-                        ],
-                      ),
-                    ),
-                  );
-                },
-                separatorBuilder: (context, index) {
-                  return Divider();
-                },
-                itemCount: 10),
+                        ),
+                        title: Text("${dataItems[index].name}"),
+                        subtitle: Text(
+                            intl.NumberFormat.simpleCurrency(locale: 'vi')
+                                .format(dataItems[index].price)),
+                        trailing: SizedBox(
+                          width: MediaQuery.of(context).size.width / 5,
+                          child: Row(
+                            children: [
+                              InkWell(onTap: () {}, child: Icon(Icons.remove)),
+                              Padding(
+                                padding:
+                                    const EdgeInsets.symmetric(horizontal: 5),
+                                child: Text(
+                                  "${dataItems[index].quantity}",
+                                  style: TextStyle(fontSize: 20),
+                                ),
+                              ),
+                              InkWell(onTap: () {}, child: Icon(Icons.add))
+                            ],
+                          ),
+                        ),
+                      );
+                    },
+                    separatorBuilder: (context, index) {
+                      return Divider();
+                    },
+                    itemCount: value.items.length);
+              },
+            ),
           ),
           Positioned(
               bottom: 0,
